12dcc723f53f18b0a602fb96e54d18731033b16e
Ben Russell <mountainbrussells@gmail.com>
2017-05-17 16:50:14 -0600

    Add timercontroller

--- BJJTV/BJJTV/BJJTVVideoViewController.swift
+++ BJJTV/BJJTV/BJJTVVideoViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class BJJTVVideoViewController: UIViewController {
+class BJJTVVideoViewController: UIViewController, YTPlayerViewDelegate {
     
     var videoID: String!
     
+    @IBOutlet weak var waitView: UIView!
     @IBOutlet weak var playerView: YTPlayerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        waitView.isHidden = true
+        playerView.delegate = self
         playerView.load(withVideoId: videoID)
+    }
+    
+    func playerView(_ playerView:YTPlayerView, didChangeTo didChangeToState:YTPlayerState) {
+            print ("\(didChangeToState)")
     }
 }
